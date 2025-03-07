54ca051266f3f2dcf01db880f7c0308f2914b7b7
unknown <naymusimran58@gmail.com>
2025-03-08 02:46:06 +0600

    added answer the questions part

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+// document.getElementById('btn-fixed').addEventListener("click", function(event){
+//     let totalTask = document.getElementById('total-task').innerText;
+//     let convertedTotalTask = parseInt(totalTask);
+//     convertedTotalTask = convertedTotalTask - 1 ;
+//     convertedTotalTask = document.getElementById('total-task').innerText = convertedTotalTask;
+//     alert("Board Update Successfully");
+//     console.log(convertedTotalTask);
+// })
